b23b635cdff84e4b6136d13b7ac7899f14d22dd7
PrakharSoni0498 <159434785+PrakharSoni0498@users.noreply.github.com>
2024-02-08 12:21:45 +0000

    code

---
+++ basic/variable.js
@@ -0,0 +1,7 @@
+const accountId = 123;
+let accountEmail = "pp@gmail.com";
+var accountPassword = "2323320";
+accountCity = "indore";
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
